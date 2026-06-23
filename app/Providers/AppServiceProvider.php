391eb312522fa6365a4214b80b35d43aca7b4844
dao <adolfo_24@yahoo.com>
2026-06-22 19:43:48 -0600

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use PhpOffice\PhpSpreadsheet\Shared\File as PhpSpreadsheetFile;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $temporaryDirectory = storage_path('framework/cache/laravel-excel');
+
+        if (! is_dir($temporaryDirectory)) {
+            mkdir($temporaryDirectory, 0775, true);
+        }
+
+        config([
+            'excel.temporary_files.local_path' => $temporaryDirectory,
+        ]);
+
+        putenv("TMPDIR={$temporaryDirectory}");
+        putenv("TEMP={$temporaryDirectory}");
+        putenv("TMP={$temporaryDirectory}");
+
+        if (function_exists('sys_get_temp_dir')) {
+            @ini_set('upload_tmp_dir', $temporaryDirectory);
+            PhpSpreadsheetFile::setUseUploadTempDirectory(true);
+        }
     }
 }
